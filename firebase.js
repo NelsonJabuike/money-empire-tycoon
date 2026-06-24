1e362367b126d4612e923232af099c954a2355e4
NelsonJabuike <charlesnicky653@gmail.com>
2026-06-24 08:45:28 +0100

    Update firebaseConfig with additional properties
    Added missing properties to firebaseConfig object.

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
    .
-   // Firebase SDK Imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import {
   getAuth
@@ -11,8 +10,11 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyAJS-efkpoLRcRfLDtINNIUOetj5bPSu80",
   authDomain: "money-empire-tycoon.firebaseapp.com",
   projectId: "money-empire-tycoon",
+  storageBucket: "money-empire-tycoon.firebasestorage.app",
+  messagingSenderId: "1064218549796",
   appId: "1:1064218549796:web:ad321be44c0bde74f0c2b1"
 };
 
