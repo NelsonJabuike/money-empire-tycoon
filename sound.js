6aa692bcc25d2d3a366f992ac8f1143d04eb3068
NelsonJabuike <charlesnicky653@gmail.com>
2026-07-04 15:43:23 +0100

    Add new audio sounds and improve playSound function

--- sound.js
+++ sound.js
@@ -11,18 +11,50 @@ buy: new Audio("sounds/buy.mp3"),
 
 reward: new Audio("sounds/reward.mp3"),
 
-achievement: new Audio("sounds/achievement.mp3")
+achievement: new Audio("sounds/achievement.mp3"),
+
+levelup: new Audio("sounds/levelup.mp3"),
+
+unlock: new Audio("sounds/unlock.mp3"),
+
+notification: new Audio("sounds/notification.mp3"),
+
+error: new Audio("sounds/error.mp3"),
+
+background: new Audio("sounds/background.mp3")
 
 };
 
 function playSound(name){
 
-if(!sounds[name]) return;
+const sound = sounds[name];
 
-sounds[name].currentTime = 0;
+if(!sound) return;
 
-sounds[name].play().catch(()=>{});
+sound.pause();
+
+sound.currentTime = 0;
+
+sound.volume = 0.5;
+
+sound.play().catch(()=>{});
 
 }
 
 window.playSound = playSound;
+
+function playBackgroundMusic(){
+
+const music = sounds.background;
+
+if(!music) return;
+
+music.loop = true;
+
+music.volume = 0.25;
+
+music.play().catch(()=>{});
+
+}
+
+window.playBackgroundMusic = playBackgroundMusic;
