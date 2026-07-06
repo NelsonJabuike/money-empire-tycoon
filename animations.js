0383957d3e7e55f341e4d7efa178e85c1a860152
NelsonJabuike <charlesnicky653@gmail.com>
2026-07-06 05:48:28 +0100

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,1 +1,22 @@
+// ==========================
+// MONEY EMPIRE TYCOON
+// Animation System
+// ==========================
 
+// Button Click Animation
+function animateClickButton(){
+
+const button =
+document.getElementById("clickBtn");
+
+if(!button) return;
+
+button.classList.remove("clickBounce");
+
+void button.offsetWidth;
+
+button.classList.add("clickBounce");
+
+}
+
+window.animateClickButton = animateClickButton;
